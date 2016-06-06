f96bb32fa2fb35240ddf95b2e6bd3a607cdf4413
otavionicollau <otavio.gatz@gmail.com>
2016-06-06 20:31:43 -0300

    update  1.9

---
+++ data/actions/scripts/otavio/eges.lua
@@ -0,0 +1,31 @@
+function onUse(player, item, frompos, item2, topos)
+
+	local monsterName = "Demon"
+	local position = player:getPosition()
+	local max = 100
+	local min = 1
+	local limite = math.random(min, max)
+	local atual = 0;
+	local porcentagem = 100
+
+	if player:getStorageValue(monsterName) >= 0  then
+		if player:getStorageValue(monsterName) <= porcentagem then
+
+				while (atual <= limite) do
+					atual = atual + 1
+					monster = Game.createMonster(monsterName, position)
+					position:sendMagicEffect(CONST_ME_MAGIC_RED)
+				end
+
+				player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Voce sumonou "..player:getStorageValue(monsterName).. "%")
+				player:setStorageValue(monsterName, player:getStorageValue(monsterName) + atual)
+
+		else
+				player:removeItem(11144,1)
+				player:setStorageValue(monsterName, 0)
+		end
+	else
+		player:setStorageValue(monsterName, 0)
+	end
+		return true
+end
